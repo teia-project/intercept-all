1766a1fdc1e198f2f110ea2152852385251d16ae
Lucas Graeff Buhl-Nielsen <lucas.buhl-nielsen@quantco.com>
2025-12-08 23:47:53 +0000

    Read arg123

--- src/bpf/main.bpf.c
+++ src/bpf/main.bpf.c
@@ -15,15 +15,22 @@ static __always_inline __u64 load_u64(const char *data, int off)
 SEC("cgroup/syscall_enter")
 int bpf_syscall_enter(struct bpf_cg_syscall_enter *ctx)
 {
+        if (ctx->nr != 49) {
+                return 1;
+        }
+
         bpf_printk("nr=%lu\n", ctx->nr);
         
-        __u64 r15 = load_u64(ctx->regs_data, 0);
-        bpf_printk("r15=%llu\n", r15);
+        __u64 arg1 = load_u64(ctx->regs_data, 14);
+        bpf_printk("arg1=%llu\n", arg1);
+        
+        __u64 arg2 = load_u64(ctx->regs_data, 15);
+        bpf_printk("arg2=%llu\n", arg2);
 
-        __u64 di = load_u64(ctx->regs_data, 14);
-        bpf_printk("di=%llu\n", di);
+        __u64 arg3 = load_u64(ctx->regs_data, 16);
+        bpf_printk("arg3=%llu\n", arg3);
 
-        ((volatile unsigned char *)&ctx->regs_data[112])[0] = 0;
+        // ((volatile unsigned char *)&ctx->regs_data[112])[0] = 0;
 
         return 1;
 }
