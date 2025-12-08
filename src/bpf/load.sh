8608d6a1e3d89a6229daf04fe93d0cc5cff22555
Oli Iliffe <oli_iliffe@hotmail.com>
2025-12-08 18:59:57 +0000

    Make code changes

--- src/bpf/load.sh
+++ src/bpf/load.sh
@@ -1,2 +1,2 @@
 bpftool prog loadall build/main.bpf.o /sys/fs/bpf/
-bpftool cgroup attach /sys/fs/cgroup cgroup_syscall_getpid pinned /sys/fs/bpf/bpf_getpid
+bpftool cgroup attach /sys/fs/cgroup cgroup_syscall_enter pinned /sys/fs/bpf/bpf_getpid
--- src/bpf/main.bpf.c
+++ src/bpf/main.bpf.c
@@ -1,9 +1,22 @@
 #include "common.h"
 
-SEC("cgroup/syscall_enter")
-int bpf_syscall_enter(struct bpf_cgroup_syscall_enter *ctx)
+// SEC("cgroup/syscall_enter")
+// int bpf_syscall_enter(struct bpf_cgroup_syscall_enter *ctx)
+// {
+//         bpf_printk("syscall(%lu)\n", ctx->nr);
+//         return 1 | 2;
+// }
+
+struct bpf_cgroup_syscall_socket {
+        __u32 family;
+        __u32 type;
+        __u32 protocol;
+        __u32 ret;
+};
+
+SEC("cgroup/syscall_socket")
+int bpf_ns_socket(struct bpf_cgroup_syscall_socket *ctx)
 {
-        bpf_printk("syscall(%lu)\n", ctx->nr);
         return 1 | 2;
 }
 
