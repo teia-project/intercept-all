1794bbe5c60262b56d5620e24c8bbbdb6bfc3b00
Oli Iliffe <oli_iliffe@hotmail.com>
2025-12-08 19:52:10 +0000

    Make code changes

--- src/bpf/main.bpf.c
+++ src/bpf/main.bpf.c
@@ -1,23 +1,23 @@
 #include "common.h"
 
-// SEC("cgroup/syscall_enter")
-// int bpf_syscall_enter(struct bpf_cgroup_syscall_enter *ctx)
-// {
-//         bpf_printk("syscall(%lu)\n", ctx->nr);
-//         return 1 | 2;
-// }
-
-struct bpf_cgroup_syscall_socket {
-        __u32 family;
-        __u32 type;
-        __u32 protocol;
-        __u32 ret;
-};
-
-SEC("cgroup/syscall_socket")
-int bpf_ns_socket(struct bpf_cgroup_syscall_socket *ctx)
+SEC("cgroup/syscall_enter")
+int bpf_syscall_enter(struct bpf_cgroup_syscall_enter *ctx)
 {
+        bpf_printk("nr=%lu\n", ctx->nr);
         return 1 | 2;
 }
+
+// struct bpf_cgroup_syscall_socket {
+//         __u32 family;
+//         __u32 type;
+//         __u32 protocol;
+//         __u32 ret;
+// };
+
+// SEC("cgroup/syscall_socket")
+// int bpf_ns_socket(struct bpf_cgroup_syscall_socket *ctx)
+// {
+//         return 1 | 2;
+// }
 
 char LICENSE[] SEC("license") = "GPL";
