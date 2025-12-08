182d48b0187f78f8f1c444ab1f8ac07cc9df86b6
Oli Iliffe <oli_iliffe@hotmail.com>
2025-12-08 20:23:44 +0000

    push somet changed

--- src/bpf/main.bpf.c
+++ src/bpf/main.bpf.c
@@ -3,7 +3,8 @@
 SEC("cgroup/syscall_enter")
 int bpf_syscall_enter(struct bpf_cgroup_syscall_enter *ctx)
 {
-        bpf_printk("nr=%lu\n", ctx->nr);
+        bpf_printk("printk blah balah \n");
+        // bpf_printk("nr=%lu\n", ctx->nr);
         return 1 | 2;
 }
 
