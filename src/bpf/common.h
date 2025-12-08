1ad48de24b5bc63e40d6c9cf1adb11f8f15d92cf
Lucas Graeff Buhl-Nielsen <lucas.buhl-nielsen@quantco.com>
2025-12-08 22:09:52 +0000

    Update program

--- src/bpf/common.h
+++ src/bpf/common.h
@@ -7,13 +7,8 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 
-struct bpf_cgroup_syscall_getpid {
-  __s32 ret;
-};
-
 struct bpf_cgroup_syscall_enter {
-        __u64 nr;
-        char donthavethelayoutforthisyet[100];
+        unsigned int nr;
 };
 
 #endif
--- src/bpf/main.bpf.c
+++ src/bpf/main.bpf.c
@@ -3,22 +3,8 @@
 SEC("cgroup/syscall_enter")
 int bpf_syscall_enter(struct bpf_cgroup_syscall_enter *ctx)
 {
-        bpf_printk("printk blah balah \n");
-        // bpf_printk("nr=%lu\n", ctx->nr);
+        bpf_printk("nr=%lu\n", ctx->nr);
         return 1 | 2;
 }
-
-// struct bpf_cgroup_syscall_socket {
-//         __u32 family;
-//         __u32 type;
-//         __u32 protocol;
-//         __u32 ret;
-// };
-
-// SEC("cgroup/syscall_socket")
-// int bpf_ns_socket(struct bpf_cgroup_syscall_socket *ctx)
-// {
-//         return 1 | 2;
-// }
 
 char LICENSE[] SEC("license") = "GPL";
