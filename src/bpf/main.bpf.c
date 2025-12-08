881fadc89c4ea400919306458f925aee7cd4e1df
Lucas Graeff Buhl-Nielsen <lucas.buhl-nielsen@quantco.com>
2025-12-08 22:24:18 +0000

    Update load and don't early return

--- src/bpf/main.bpf.c
+++ src/bpf/main.bpf.c
@@ -4,7 +4,7 @@ SEC("cgroup/syscall_enter")
 int bpf_syscall_enter(struct bpf_cgroup_syscall_enter *ctx)
 {
         bpf_printk("nr=%lu\n", ctx->nr);
-        return 1 | 2;
+        return 1;
 }
 
 char LICENSE[] SEC("license") = "GPL";
