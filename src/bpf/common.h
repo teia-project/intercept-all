117f0525e875a170a5289b0f686fcaf1e8553454
Oli Iliffe <oli_iliffe@hotmail.com>
2025-12-09 18:31:58 +0000

    Use new struct

--- src/bpf/common.h
+++ src/bpf/common.h
@@ -8,9 +8,10 @@
 #include <bpf/bpf_tracing.h>
 
 struct bpf_cg_syscall_enter {
-  unsigned int nr;
-  char regs_data[512];
-  __s32 ret;
+	unsigned int nr;
+	unsigned long arg0, arg1, arg2, arg3, arg4, arg5, ret;
+	__u8 resolve_ptr_regs;
+	__u64 scratch[128];
 };
 
 #endif
--- src/bpf/main.bpf.c
+++ src/bpf/main.bpf.c
@@ -1,16 +1,16 @@
 #include "common.h"
 
-static __always_inline __u64 load_u64(const char *data, int off)
-{
-    return ((__u64)(unsigned char)data[(off * 8) + 0])        |
-           ((__u64)(unsigned char)data[(off * 8) + 1] << 8)  |
-           ((__u64)(unsigned char)data[(off * 8) + 2] << 16) |
-           ((__u64)(unsigned char)data[(off * 8) + 3] << 24) |
-           ((__u64)(unsigned char)data[(off * 8) + 4] << 32) |
-           ((__u64)(unsigned char)data[(off * 8) + 5] << 40) |
-           ((__u64)(unsigned char)data[(off * 8) + 6] << 48) |
-           ((__u64)(unsigned char)data[(off * 8) + 7] << 56);
-}
+// static __always_inline __u64 load_u64(const char *data, int off)
+// {
+//     return ((__u64)(unsigned char)data[(off * 8) + 0])        |
+//            ((__u64)(unsigned char)data[(off * 8) + 1] << 8)  |
+//            ((__u64)(unsigned char)data[(off * 8) + 2] << 16) |
+//            ((__u64)(unsigned char)data[(off * 8) + 3] << 24) |
+//            ((__u64)(unsigned char)data[(off * 8) + 4] << 32) |
+//            ((__u64)(unsigned char)data[(off * 8) + 5] << 40) |
+//            ((__u64)(unsigned char)data[(off * 8) + 6] << 48) |
+//            ((__u64)(unsigned char)data[(off * 8) + 7] << 56);
+// }
 
 SEC("cgroup/syscall_enter")
 int bpf_syscall_enter(struct bpf_cg_syscall_enter *ctx)
@@ -20,26 +20,19 @@ int bpf_syscall_enter(struct bpf_cg_syscall_enter *ctx)
         }
 
         bpf_printk("nr=%lu\n", ctx->nr);
-        
-        __u64 arg1 = load_u64(ctx->regs_data, 14);
-        bpf_printk("arg1=%llu\n", arg1);
-        
-        __u64 arg2 = load_u64(ctx->regs_data, 13);
-        bpf_printk("arg2=%llu\n", arg2);
-
-        __u64 arg3 = load_u64(ctx->regs_data, 12);
-        bpf_printk("arg3=%llu\n", arg3);
+                
+        bpf_printk("arg0=%llu\n", ctx->arg0);
+        bpf_printk("ctx->arg1=%zx\n", ctx->arg1);
+        bpf_printk("ctx->arg2=%llu\n", ctx->arg2);
 
         struct sockaddr_in addr;
-        if (bpf_probe_read_user(&addr, sizeof(addr), (void *) arg2) < 0) {
+        if (bpf_probe_read_user(&addr, sizeof(addr), (void *)ctx->arg1) < 0) {
                 return 0;
         }
 
         bpf_printk("sin_family=%u\n", addr.sin_family);
         bpf_printk("sin_addr=%u\n", bpf_ntohl(addr.sin_addr.s_addr));
         bpf_printk("sin_port=%u\n", bpf_ntohs(addr.sin_port));
-
-        // ((volatile unsigned char *)&ctx->regs_data[112])[0] = 0;
 
         return 1;
 }
