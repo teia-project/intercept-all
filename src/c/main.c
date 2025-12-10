114b68b2bbf7027963f5e47114db6ff6e9fdecf9
Oli Iliffe <oli_iliffe@hotmail.com>
2025-12-10 16:47:34 +0000

    Print intercepted arg before/after

--- src/c/main.c
+++ src/c/main.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <string.h>
+#include <sys/syscall.h>
+#include <unistd.h>
 
 static inline unsigned long rdi(void) {
   unsigned long val;
@@ -16,12 +17,24 @@ static inline unsigned long rsi(void) {
 }
 
 int main() {
-  //   asm volatile("movq %0, %%rdi" : : "r"(0xEB9Ful) : "rdi");
-  //   volatile int n = getpid();
-  //   printf("rdi=%zu, rsi=%zu\n", rdi(), rsi());
-  int fd = open("eps-is-not-general", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  char const *path = "eps-is-not-general";
+  printf("path(before)=%p\n", path);
+  int flags = O_WRONLY | O_CREAT | O_TRUNC;
+  int perms = 0644;
+  long fd;
+  asm volatile("mov %[perms], %%r10\n\t" // 4th arg
+               "mov %[nr], %%rax\n\t"    // syscall number
+               "syscall"
+               : "=a"(fd)
+               : [nr] "i"(SYS_openat),
+                 "D"(AT_FDCWD), // 1st arg
+                 "S"(path),     // 2nd arg
+                 "d"(flags),    // 3rd arg
+                 [perms] "r"((long)perms)
+               : "rcx", "r11", "r10", "memory");
   if (fd < 0)
     return 1;
+  printf("path(after) =%p\n", (void *)rsi());
 
   const char *msg = "I think I can get away with naming files lies\n";
   write(fd, msg, strlen(msg));
