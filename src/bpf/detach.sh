c4a7ae92b8e068018eb36ad658e2afaf4ce4ae6d
Oli Iliffe <oli_iliffe@hotmail.com>
2025-12-11 20:11:23 +0000

    something

--- src/bpf/detach.sh
+++ src/bpf/detach.sh
@@ -1,2 +1,4 @@
 bpftool cgroup detach /sys/fs/cgroup cgroup_syscall_enter pinned /sys/fs/bpf/bpf_syscall_enter
 rm -r /sys/fs/bpf/bpf_syscall_enter
+bpftool cgroup detach /sys/fs/cgroup cgroup_syscall_exit pinned /sys/fs/bpf/bpf_syscall_exit
+rm -r /sys/fs/bpf/bpf_syscall_exit
